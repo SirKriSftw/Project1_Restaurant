a345b27084bbd55d7ac06bf98eeb46fbe8fab812
Kristian Perez <kpere192@fiu.edu>
2022-03-29 16:51:05 -0400

    Added add User, get Users and get User by ID API CALLS (and removed starter code from template)

---
+++ RestAPP/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using RestAPP.Models;
+
+namespace RestAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserModel model = new UserModel();
+
+        [HttpGet]
+        [Route("ulist")]
+        public IActionResult userList()
+        {
+            try
+            {
+                return Ok(model.getUsers());
+            }
+            catch (Exception ex)
+            { 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("uid")]
+        public IActionResult userByID(int userID)
+        {
+            try
+            {
+                return Ok(model.getUser(userID));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("adduser")]
+        public IActionResult addUser(UserModel newUser)
+        {
+            try
+            {
+                return Created("",model.addUser(newUser));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ RestAPP/Models/UserModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace RestAPP.Models
+{
+    public class UserModel
+    {
+        #region Columns / Properties
+        public int userID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public bool isAdmin { get; set; }
+        #endregion
+        SqlConnection conn = new SqlConnection(@"server=DESKTOP-TDTK0RJ\KRISSQL;Initial Catalog=RestaurantProj1;Persist Security Info=True;User ID=sa;Password=rev511");
+
+        public List<UserModel> getUsers()
+        {
+            string query = "SELECT * FROM Users";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            List<UserModel> users = new List<UserModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    users.Add(new UserModel()
+                    {
+                        userID = reader.GetInt32(0),
+                        name = reader.GetString(3),
+                        isAdmin = reader.GetBoolean(4)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return users;
+        }
+
+        public UserModel getUser(int userID)
+        {
+            string query = "SELECT * FROM Users WHERE userID = @userID";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@userID",userID);
+            UserModel user = new UserModel();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    user.userID = userID;
+                    user.name = reader.GetString(3);
+                    user.isAdmin = reader.GetBoolean(4);
+                }
+                else
+                {
+                    throw new Exception("User not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return user;
+        }
+
+        public string addUser(UserModel newUser)
+        {
+            string query = "INSERT INTO Users VALUES(@username, @password, @name, @isAdmin)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@username",newUser.username);
+            cmd.Parameters.AddWithValue("@password", newUser.password);
+            cmd.Parameters.AddWithValue("@name", newUser.name);
+            cmd.Parameters.AddWithValue("@isAdmin", 0);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return "User " + newUser.name + " added successfully";
+        }
+    }
+}
