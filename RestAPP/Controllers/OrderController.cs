f8faf034d734cafbacfe9addf5483e771afb3cb5
Kristian Perez <kpere192@fiu.edu>
2022-03-30 15:21:43 -0400

    Added GET orders by resID, userID and all

---
+++ RestAPP/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using RestAPP.Models;
+
+namespace RestAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        OrderModel model = new OrderModel();
+
+        [HttpGet]
+        [Route("olist")]
+        public IActionResult OrderList()
+        {
+            try
+            {
+                return Ok(model.GetOrders());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("oRlist")]
+        public IActionResult OrderByRes(int resID)
+        {
+            try
+            {
+                return Ok(model.GetReservationOrders(resID));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("oUlist")]
+        public IActionResult OrderByUser(int userID)
+        {
+            try
+            {
+                return Ok(model.GetUserOrders(userID));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
--- RestAPP/Models/OrderModel.cs
+++ RestAPP/Models/OrderModel.cs
@@ -12,5 +12,112 @@ namespace RestAPP.Models
         public double cost { get; set; }
         #endregion
         SqlConnection conn = new SqlConnection(@"server=DESKTOP-TDTK0RJ\KRISSQL;Initial Catalog=RestaurantProj1;Persist Security Info=True;User ID=sa;Password=rev511");
-    }
+
+        public List<OrderModel> GetOrders()
+        {
+            string query = "SELECT * FROM Orders";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            List<OrderModel> orders = new List<OrderModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    orders.Add(new OrderModel()
+                    {
+                        orderID = reader.GetInt32(0),
+                        resID = reader.GetInt32(1),
+                        cost = reader.GetDouble(2),
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return orders;
+        }
+
+        public List<OrderModel> GetReservationOrders(int resID)
+        {
+            string query = "SELECT * FROM Orders WHERE resID=@resID";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@resID",resID);
+            List<OrderModel> orders = new List<OrderModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    orders.Add(new OrderModel()
+                    {
+                        orderID = reader.GetInt32(0),
+                        resID = resID,
+                        cost = reader.GetDouble(2),
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return orders;
+        }
+
+        public List<OrderModel> GetUserOrders(int userID)
+        {
+            string query = "SELECT * FROM Orders WHERE resID = (SELECT resID FROM Users WHERE userID=@userID)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@userID",userID);
+            List<OrderModel> orders = new List<OrderModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    orders.Add(new OrderModel()
+                    {
+                        orderID = reader.GetInt32(0),
+                        resID = reader.GetInt32(1),
+                        cost = reader.GetDouble(2),
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return orders;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }    
 }
+
+
