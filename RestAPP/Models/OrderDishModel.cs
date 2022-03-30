aeacfd523b46de10252e3df4672d1eda5769fab3
Kristian Perez <kpere192@fiu.edu>
2022-03-30 16:09:49 -0400

    Added Ordered Dishes model methods for GET by: ALL, orderID, resID, and userID

--- RestAPP/Models/OrderDishModel.cs
+++ RestAPP/Models/OrderDishModel.cs
@@ -12,5 +12,136 @@ namespace RestAPP.Models
         public int quantity { get; set; }
         #endregion
         SqlConnection conn = new SqlConnection(@"server=DESKTOP-TDTK0RJ\KRISSQL;Initial Catalog=RestaurantProj1;Persist Security Info=True;User ID=sa;Password=rev511");
+
+        public List<OrderDishModel> GetOrderedDishes()
+        {
+            string query = "SELECT * FROM OrderedDishes";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            List<OrderDishModel> orderedDishes = new List<OrderDishModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                    throw new Exception("No ordered dishes found");
+                while (reader.Read())
+                {
+                    orderedDishes.Add(new OrderDishModel()
+                    {
+                        orderID = reader.GetInt32(0),
+                        dishID = reader.GetInt32(1),
+                        quantity = reader.GetInt32(2)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return orderedDishes;
+        }
+
+        public List<OrderDishModel> GetOrdersDishes(int orderID)
+        {
+            string query = "SELECT * FROM OrderedDishes WHERE orderID=@orderID";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@orderID",orderID);
+            List<OrderDishModel> orderedDishes = new List<OrderDishModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                    throw new Exception("No ordered dishes found");
+                while (reader.Read())
+                {
+                    orderedDishes.Add(new OrderDishModel()
+                    {
+                        orderID = reader.GetInt32(0),
+                        dishID = reader.GetInt32(1),
+                        quantity = reader.GetInt32(2)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return orderedDishes;
+        }
+
+        public List<OrderDishModel> GetResDishes(int resID)
+        {
+            string query = "SELECT * FROM OrderedDishes WHERE orderID = (SELECT orderID FROM Orders WHERE resID=@resID)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@resID", resID);
+            List<OrderDishModel> orderedDishes = new List<OrderDishModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                    throw new Exception("No ordered dishes found");
+                while (reader.Read())
+                {
+                    orderedDishes.Add(new OrderDishModel()
+                    {
+                        orderID = reader.GetInt32(0),
+                        dishID = reader.GetInt32(1),
+                        quantity = reader.GetInt32(2)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return orderedDishes;
+        }
+
+        public List<OrderDishModel> GetUserDishes(int userID)
+        {
+            string query = "SELECT * FROM OrderedDishes WHERE orderID = (SELECT orderID FROM Orders WHERE resID=(SELECT resID FROM Reservations WHERE userID=@userID))";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@userID", userID);
+            List<OrderDishModel> orderedDishes = new List<OrderDishModel>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                    throw new Exception("No ordered dishes found");
+                while (reader.Read())
+                {
+                    orderedDishes.Add(new OrderDishModel()
+                    {
+                        orderID = reader.GetInt32(0),
+                        dishID = reader.GetInt32(1),
+                        quantity = reader.GetInt32(2)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return orderedDishes;
+        }
     }
 }
