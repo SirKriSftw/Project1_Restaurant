ba65fd805b947e636816ab29ec97e21cf19a7044
Kristian Perez <kpere192@fiu.edu>
2022-04-01 15:53:59 -0400

    Started adding Serilog

--- RestAPP/Program.cs
+++ RestAPP/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace RestAPP
 {
@@ -13,7 +14,27 @@ namespace RestAPP
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configSettings = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configSettings)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application starting");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application failed to start correctly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
